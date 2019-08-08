b0f51f46d31efacc579ca7c335f0bc4090df5bc6
Kira Sl <42243887+KiraSl@users.noreply.github.com>
2019-08-08 07:57:04 +0100

    Make the first working example

--- scripts.js
+++ scripts.js
@@ -1,46 +1,40 @@
-// set default global variables
-let selectedOperator;
-let clickedNumber;
-let previousResult;
-let operatorClicked = false;
+let currentNumber = 0;
+let equation;
 
-function selectOperator(el) {
-  // set `selectedOperator` to selecyed operator
-  selectedOperator = el.getAttribute('data-attr');
-  if (clickedNumber && operatorClicked && previousResult) {
-    // Evaluate the equasion of prevResult, operator and clickedNr eg. "7+3"
-    const result = eval(`${previousResult}${selectedOperator}${clickedNumber}`);
-    // Assign the result the innterText of the HTML element with the `ID` result 
-    document.querySelector('#result').innerText = result;
+function selectNumber(number) {
+  if (equation) {
+    equation = `${equation}${number}`;
   }
-
-  // set `operatorClicked` flag to `true`
-  operatorClicked = true;
+  if (currentNumber) {
+    currentNumber = `${currentNumber}${number}`;
+  } else {
+    currentNumber = number;
+  }
+  document.querySelector('#result').innerText = currentNumber;
+  console.log(equation);
 }
 
-function selectNumber(el) { // eg. new clicked number is 7
-  if (!clickedNumber || operatorClicked) {
-    // If we did no click a number OR clicked on an operaotor, 
-    // set `clickedNumber` to the number we clieked on.
-    clickedNumber = el.getAttribute('data-attr');
+function selectOperator(operator) {
+  let lastCharacter = equation && equation.charAt(equation.length - 1);
+  let characterIsNotNumber = isNaN(Number(lastCharacter));
+  if (!characterIsNotNumber) {
+    calculate();
+    equation = `${equation}${operator}`;
+  } else if (currentNumber) {
+    equation = `${currentNumber}${operator}`;
   } else {
-    // else concatinate the previous clicked number with new cliecked number
-    // eg. previous clicked number is 2, new clicked number is 7, result will be 27
-    clickedNumber = `${clickedNumber}${el.getAttribute('data-attr')}`;
+    equation = `${equation.slice(0, -1)}${operator}`;
   }
+  currentNumber = 0;
+  console.log(equation);
+  
+}
 
-  // set previous result to 
-  previousResult = document.querySelector('#result').innerText;
-
-  // assign the clickedNr value to yhe innerText of the HTML
-  // eg. 7 or 27
-  document.querySelector('#result').innerText = clickedNumber;
-
+function calculate() {
+  const result = eval(equation);
+  document.querySelector('#result').innerText = result || '0';
 }
 
 function reset() {
-  // reset the HTML to 0 and all global variables
-  document.querySelector('#result').innerText = "0";
-  selectedOperator = undefined;
-  clickedNumber = undefined;
+  
 }
